3e6f418c9af1c4e923b9f80cd891af85a09d8fc6
weasdown <34220924+weasdown@users.noreply.github.com>
2025-09-01 15:40:26 +0100

    feat: add placeholder razel() function

--- razel/razel.py
+++ razel/razel.py
@@ -1,5 +1,19 @@
 # Calculates spacecraft azimuth and elevation from a site on the Earth.
 # For full details, see Algorithm 27 (RAZEL) on page 265 of Fundamentals of Astrodynamics and Applications (4th ed.) by David A Vallado.
 
+def razel():
+    """
+    Calculates a spacecraft's range, azimuth, elevation, and the rates of these, from a point on the ground.
+    
+    Inputs:
+    r_ECI, v_ECI, yr, mo, day, UTC, ΔUT1, ΔAT, x_p, y_p, φ_gd, λ, h_ellp
+
+    Outputs: 
+    ρ, β, el, ρ_dot, β_dot, el_dot
+
+    Based on Algorithm 27 on page 265 of Fundamentals of Astrodynamics and Applications (4th ed.) by David A Vallado.
+    """
+    pass
+
 if __name__ == '__main__':
     pass
