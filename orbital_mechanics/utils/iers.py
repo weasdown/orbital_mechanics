e21105b9f8122ef6d2ee2f96120b61ddd3bf3fec
weasdown <34220924+weasdown@users.noreply.github.com>
2025-09-02 16:15:11 +0100

    feat: add IERS.bulletin_c(), bulletin_d() methods

--- orbital_mechanics/utils/iers.py
+++ orbital_mechanics/utils/iers.py
@@ -1,3 +1,5 @@
+import requests as r
+
 class IERS:
     """Class for getting time correction values and Earth rotation poles from IERS."""
 
@@ -16,3 +18,17 @@ class IERS:
     @staticmethod
     def y_p():
         pass
+
+    @staticmethod
+    def bulletin_c() -> str:
+        """Gets the latest Bulletin C."""
+        url: str = 'https://datacenter.iers.org/data/latestVersion/bulletinC.txt'
+        resp: r.Response = r.get(url)
+        return resp.text
+
+    @staticmethod
+    def bulletin_d():
+        """Gets the latest Bulletin D."""
+        url: str = 'https://datacenter.iers.org/data/latestVersion/bulletinD.txt'
+        resp: r.Response = r.get(url)
+        return resp.text
