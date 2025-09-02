492d5d5b1feea265ee8ed882e3434d83de2b1758
weasdown <34220924+weasdown@users.noreply.github.com>
2025-09-02 16:08:07 +0100

    feat: add placeholder staticmethods IERS.d_at(), d_ut1(), x_p(), y_p()

--- orbital_mechanics/utils/iers.py
+++ orbital_mechanics/utils/iers.py
@@ -1,3 +1,18 @@
 class IERS:
     """Class for getting time correction values and Earth rotation poles from IERS."""
-    pass
+
+    @staticmethod
+    def d_at(self):
+        pass
+
+    @staticmethod
+    def d_ut1(self):
+        pass
+
+    @staticmethod
+    def x_p(self):
+        pass
+
+    @staticmethod
+    def y_p(self):
+        pass
