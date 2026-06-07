1fe08cd5ccccd867eeb1fa631e6df37e61a58833
weasdown <34220924+weasdown@users.noreply.github.com>
2026-06-07 18:44:01 +0100

    fix: remove redundant commented code

--- examples/iers_example.py
+++ examples/iers_example.py
@@ -2,9 +2,6 @@ import orbital_mechanics as om
 
 iers = om.IERS()
 
-# print(f'Bulletin C:\n{iers.bulletin_c()}\n\n')
-# print(f'Bulletin D:\n{iers.bulletin_d()}\n\n')
-#
 print(f'ΔUT1 from IERS: {iers.d_ut1()}')
 print(f'ΔAT from IERS: {iers.d_at()}')
 
