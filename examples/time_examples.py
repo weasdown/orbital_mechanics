a7be8f0535ece5f66a4d3f023c44ac1f05b8b5ed
weasdown <34220924+weasdown@users.noreply.github.com>
2026-06-07 20:47:10 +0100

    feat: demonstrate preliminary implementation of conv_time()

--- examples/time_examples.py
+++ examples/time_examples.py
@@ -2,5 +2,11 @@ from datetime import datetime
 
 from orbital_mechanics import conv_time
 
-outputs = conv_time(date_time=datetime(2004, 5, 14, 16, 43), d_ut1=-0.463326, d_at=32)
-print(outputs)
+ut1, tai, tt, tdb, t_ut1, t_tt, t_tdb = conv_time(date_time=datetime(2004, 5, 14, 16, 43), d_ut1=-0.463326, d_at=32)
+print(f'ut1 = {ut1.strftime("%H:%M:%S.%f")}\n'
+      f'tai = {tai.strftime("%H:%M:%S.%f")}\n'
+      f'tt = {tt.strftime("%H:%M:%S.%f")}\n'
+      f'tdb = {tdb.strftime("%H:%M:%S.%f")}\n'
+      f'{t_ut1 = }\n'
+      f'{t_tt = }\n'
+      f'{t_tdb = }\n')
