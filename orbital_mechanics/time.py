fa40c73b317c496751e038edca819954adcbeb26
weasdown <34220924+weasdown@users.noreply.github.com>
2026-06-07 20:46:57 +0100

    feat: add preliminary implementation of conv_time()

--- orbital_mechanics/time.py
+++ orbital_mechanics/time.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from math import sin
 
 
 # TODO implement conv_time() function
@@ -17,16 +18,46 @@ def conv_time(date_time: datetime, d_ut1: float, d_at: int) -> list:
 
     .. _IERS: https://www.iers.org/IERS/EN/Home
     """
-    ut1 = date_time + timedelta(days=0, seconds=d_ut1)
-    tai = ...
-    tt = ...
-    tdb = ...
-    t_uti = ...
-    t_tt = ...
-    t_tdb = ...
 
-    raise NotImplementedError('conv_time() function is not yet implemented.')
-    # return [ut1, tai, tt, tdb, t_uti, t_tt, t_tdb]
+    utc = date_time.time()
+    ut1: datetime = date_time + timedelta(days=0, seconds=d_ut1)
+
+    # TODO add check that utc and ut1 are within 0.9 seconds.
+
+    tai: datetime = date_time + timedelta(days=0, seconds=d_at)
+
+    # gps: datetime = date_time + timedelta(days=0, seconds=d_at - 19)
+
+    tt: datetime = tai + timedelta(days=0, seconds=32.184)
+
+    jd_tt: float = julian_date(datetime(date_time.year, date_time.month, date_time.day, tt.hour, tt.minute, tt.second))
+
+    t_tt: float = (jd_tt - 2451545.0) / 36525
+
+    # jd_tai: float = julian_date(
+    #     datetime(date_time.year, date_time.month, date_time.day, tai.hour, tai.minute, tai.second))
+
+    # Hardcoded values copied from Algorithm 16 in Vallado.
+    tdb: datetime = tt + timedelta(days=0, seconds=0.001657 * sin(628.3076 * t_tt + 6.2401)
+                                                   + 0.000022 * sin(575.3385 * t_tt + 4.2970)
+                                                   + 0.000014 * sin(1256.6152 * t_tt + 6.1969)
+                                                   + 0.000005 * sin(606.9777 * t_tt + 4.0212)
+                                                   + 0.000005 * sin(52.9691 * t_tt + 0.4444)
+                                                   + 0.000002 * sin(21.3299 * t_tt + 5.5431)
+                                                   + 0.000010 * t_tt * sin(628.3076 * t_tt + 4.2490))
+
+    jd_tdb: float = julian_date(
+        datetime(date_time.year, date_time.month, date_time.day, tdb.hour, tdb.minute, tdb.second))
+
+    jd_ut1: float = julian_date(
+        datetime(date_time.year, date_time.month, date_time.day, ut1.hour, ut1.minute, ut1.second))
+
+    # tdb: float = (jd_tt - 2451545.0) / 36525
+    t_ut1: float = (jd_ut1 - 2451545.0) / 36525
+
+    t_tdb: float = (jd_tdb - 2451545.0) / 36525
+
+    return [ut1, tai, tt, tdb, t_ut1, t_tt, t_tdb]
 
 
 def julian_date(date_time: datetime) -> float:
