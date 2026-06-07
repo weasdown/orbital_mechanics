a47a1cb3664e3a31ed07eb4e81fef28d86fd87dc
weasdown <34220924+weasdown@users.noreply.github.com>
2026-06-07 18:59:16 +0100

    style: auto-formatting

--- orbital_mechanics/razel/razel.py
+++ orbital_mechanics/razel/razel.py
@@ -8,7 +8,8 @@ from orbital_mechanics.utils.rotations import rot2, rot3
 
 
 # TODO add missing argument type hints once types known.
-def razel(r_eci: np.ndarray, v_eci: np.ndarray, date_time: datetime, d_ut1: float, d_at: int, x_p: float, y_p: float, phi_gd: float, lda: float, h_ellp: float) -> list[float]:
+def razel(r_eci: np.ndarray, v_eci: np.ndarray, date_time: datetime, d_ut1: float, d_at: int, x_p: float, y_p: float,
+          phi_gd: float, lda: float, h_ellp: float) -> list[float]:
     """Calculates a spacecraft's range, azimuth, elevation, and the rates of these, from an observation site on the ground.
 
     :param r_eci: spacecraft position vector in the Earth-centred inertial (ECI) reference frame.
@@ -52,7 +53,7 @@ def razel(r_eci: np.ndarray, v_eci: np.ndarray, date_time: datetime, d_ut1: floa
     r_site_ecef: np.ndarray = site(phi_gd, lda, h_ellp)
 
     rho_ecef: np.ndarray = r_ecef - r_site_ecef
-    
+
     rho_dot_ecef: np.ndarray = v_ecef
 
     rot2_90_minus_phi_gd: np.ndarray = rot2(90 - phi_gd)
@@ -62,11 +63,11 @@ def razel(r_eci: np.ndarray, v_eci: np.ndarray, date_time: datetime, d_ut1: floa
     cos_phi_gd: float = cos(phi_gd)
     sin_lambda: float = sin(lda)
     cos_lambda: float = cos(lda)
-    sez_over_ecef: np.ndarray = np.array([[sin_phi_gd * cos_lambda, sin_phi_gd * sin_lambda, -cos_phi_gd], 
+    sez_over_ecef: np.ndarray = np.array([[sin_phi_gd * cos_lambda, sin_phi_gd * sin_lambda, -cos_phi_gd],
                                           [-sin_lambda, cos_lambda, 0],
                                           [cos_phi_gd * cos_lambda, cos_phi_gd * sin_lambda, sin_phi_gd]])
 
-    assert(rot2_90_minus_phi_gd * rot3_lambda == sez_over_ecef)
+    assert (rot2_90_minus_phi_gd * rot3_lambda == sez_over_ecef)
 
     rho_sez: np.ndarray = sez_over_ecef * rho_ecef
     rho_dot_sez: np.ndarray = sez_over_ecef * rho_dot_ecef
@@ -80,7 +81,6 @@ def razel(r_eci: np.ndarray, v_eci: np.ndarray, date_time: datetime, d_ut1: floa
     rho_dot_s: float = float(rho_dot_sez[0])  # S component of rho dot SEZ vector
     rho_dot_e: float = float(rho_dot_sez[1])  # E component of rho dot SEZ vector
     rho_dot_z: float = float(rho_dot_sez[2])  # Z component of rho dot SEZ vector
-
 
     # Book says rho_z rather than rho_sez for first value but unclear what rho_z is.
     sin_el: float = float(rho_sez / rho)
@@ -108,7 +108,8 @@ def razel(r_eci: np.ndarray, v_eci: np.ndarray, date_time: datetime, d_ut1: floa
 
 
 # TODO add missing argument type hints once types known.
-def fk5(r_eci: np.ndarray, v_eci: np.ndarray, date_time: datetime, d_ut1: float, d_at: int, x_p: float, y_p: float) -> list[np.ndarray]:
+def fk5(r_eci: np.ndarray, v_eci: np.ndarray, date_time: datetime, d_ut1: float, d_at: int, x_p: float, y_p: float) -> \
+        list[np.ndarray]:
     """
     Converts position and velocity vectors in the ECI frame to the ECEF frame.
 
@@ -149,7 +150,7 @@ def site(phi_gd: float, lda: float, h_ellp: float) -> np.ndarray:
     lda (λ): longitude in degrees, with West being negative.
     h_ellp: height above mean sea level in metres.
     """
-    
+
     raise NotImplementedError
 
     # # FIXME fix placeholder value for output.
