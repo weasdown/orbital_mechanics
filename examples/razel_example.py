06b47d387c7532774a1bb8915d7d42b125dcf5f8
weasdown <34220924+weasdown@users.noreply.github.com>
2026-06-07 19:31:54 +0100

    refactor: get d_ut1, d_at, x_p, y_p from IERS

--- examples/razel_example.py
+++ examples/razel_example.py
@@ -8,13 +8,10 @@ iss_r_eci = np.array([])
 iss_v_eci = np.array([])
 
 iers = IERS()
-poles = iers.poles()
-x_p = poles[0]
-y_p = poles[1]
 
 lat = 51.761146
 long = -1.255242
 height = 50
 
-outputs = razel(iss_r_eci, iss_v_eci, datetime.now(), iers.d_ut1(), iers.d_at(), x_p, y_p, lat, long, height)
+outputs = razel(iss_r_eci, iss_v_eci, datetime.now(), lat, long, height)
 print(outputs)
